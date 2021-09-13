7f7059de977a69623e30f4821eef86662a1dcc47
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-09-13 13:20:14 +0300

    fixed validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -49,7 +49,7 @@ const validateCreateUser = celebrate({
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    name: Joi.string().required().min(8),
+    password: Joi.string().required().min(8),
   }),
 });
 
