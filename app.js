4a388d1c819cf50a3923aba4fffa634d09a7f7b5
Irina Aleksandrova <myr-irina2@yandex.ru>
2021-09-14 20:43:30 +0300

    added cors

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { MONGO_URL } = require('./utils/secretKey');
 const errorHandler = require('./middlewares/errorHandler');
-// const cors = require('cors');
 const { limiter } = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -16,6 +16,17 @@ const router = require('./routes/index');
 
 mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(
+  cors({
+    origin: [
+      'https://films-express.nomoredomains.club',
+      'http://films-express.nomoredomains.club',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  }),
+);
 app.use(requestLogger);
 app.use(express.json());
 app.use(express.urlencoded({
